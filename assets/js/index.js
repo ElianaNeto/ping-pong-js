164f087939cd59c95a7a7efb8e436f6152d9397f
Eliana Neto <eliananeto29@gmail.com>
2022-11-06 00:20:22 +0100

    Main part

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
 var gameMode;
+var home = document.getElementById('home');
+var game = document.getElementById('game');
+function displayHome(){
+  game.style.display = "none";
+  home.style.display = "block";
+}
+
+function displayGame(){
+  game.style.display = "block";
+  home.style.display = "none";
+}
+
 function mode(element, mode){
   gameMode = mode;
-  window.open('./assets/html/game.html','_self');
+  //window.open('./assets/html/game.html','_self');
   console.log(gameMode);
+  displayGame();
 }
+
