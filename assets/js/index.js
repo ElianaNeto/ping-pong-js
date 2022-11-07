3bb9535fcd8e5b78fab3519e07cc25d730f2fb88
Eliana Neto <eliananeto29@gmail.com>
2022-11-07 17:00:23 +0100

    update: troca entre modos de jogo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,45 @@
 var gameMode;
 var home = document.getElementById('home');
 var game = document.getElementById('game');
+var falgStartGame = false;
+
 function displayHome(){
+  location.reload();
   game.style.display = "none";
   home.style.display = "block";
+  falgStartGame = false;
+  console.log("JOGO ACABOU")
+
 }
 
-function displayGame(){
+function displayGame(mode){
   game.style.display = "block";
   home.style.display = "none";
+  falgStartGame = true;
+  if(falgStartGame){
+   startGame(mode);
+   console.log("JOGO COMECOU")
+  }
+
 }
 
 function mode(element, mode){
   gameMode = mode;
   //window.open('./assets/html/game.html','_self');
   console.log(gameMode);
-  displayGame();
+  displayGame(gameMode);
+}
+
+function startGame(mode){
+
+  if(mode === 'hh'){
+    console.log("modo humano")
+    modeHumanHuman();
+  }
+  else{
+    console.log("mode computador")
+    modeHumanComputer();
+  }
+
 }
 
